351b1b8e4607eaed56199e0a2a2713a48bd82ec4
Jakub Dranczewski <domdlapsa@gmail.com>
2019-07-23 17:00:22 +0200

    Visit pushing test

--- SimpleWebStats.php
+++ SimpleWebStats.php
@@ -1,8 +1,43 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-if (isset($_POST["referrer"])) {
+if (isset($_POST["visit"])) {
     header("HTTP/1.0 204 No Content");
+
+    // Options
+    $servername = "server";
+    $username = "username";
+    $password = "password";
+    $dbname = "myDB";
+    // Additional column names. id and timestamp are automatic.
+    $columns = ["example"];
+    $column_vals = [];
+
+    // Establish a connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Construct the query
+    $sql = "INSERT INTO SimpleWebStats (";
+    $bind = "";
+    foreach ($columns as $value) {
+        $sql += $value . ", ";
+        $bind += "s";
+        array_push($column_vals, $_POST[$value]);
+    }
+    $sql += ") VALUES (";
+    foreach ($columns as $value) {
+        $sql += "?, ";
+    }
+    $sql += ")";
+
+    // Prepare statement and bind parameters
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param($bind, ...$column_vals);
+
+    $conn->close();
 } else { ?>
     This is a simple, non-intrusive, GDPR-compliant website analytics script.
     It collects only the time of visit and simple metadata that do not identify
